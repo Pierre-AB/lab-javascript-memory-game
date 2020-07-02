66df36d6050cbfe48cfa0627c7f486a266c519c9
Pierre-Alderic Boegler <pa.boegler@gmail.com>
2020-07-02 21:34:18 +0200

    iteration 2 done

--- js/memory.js
+++ js/memory.js
@@ -1,25 +1,42 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
     this.pickedCards = [];
-    this.pairClicked = 0
-    this.pairsGuessed= 0;
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
   shuffleCards() {
+    // this.cards.sort(function () {
+    //   return Math.random() - .5; // [-.5----.5]
+    // })
     var i, tmp;
-    console.log(i, tmp)
+    console.log(i, tmp);
     for (i = this.cards.length - 1; i > 0; i--) {
-        randomIndex = Math.floor(Math.random() * (i + 1));
-        console.log(randomIndex)
-        tmp = this.cards[i];
-        console.log(this.cards[i])
-        this.cards[i] = this.cards[randomIndex];
-        this.cards[randomIndex] = tmp;
+      let randomIndex = Math.floor(Math.random() * (i + 1));
+      tmp = this.cards[i];
+      this.cards[i] = this.cards[randomIndex];
+      this.cards[randomIndex] = tmp;
     }
     return this.cards;
-    
   }
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+
+  checkIfPair(card1, card2) {
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      this.pairsClicked++;
+      return false;
+    }
+  }
+  isFinished() {
+    // this.pairsGuessed === 12 ? true : false
+
+    if (this.pairsGuessed === 12) {
+      return true
+    }
+    return false
+  }
 }
