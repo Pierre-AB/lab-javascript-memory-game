0b9ab71227b40eaee1bde74cc2729b24ae36f628
Lekmiti Wafaa <lekmiti.wafaa@gmail.com>
2020-07-02 21:11:23 +0200

    iteration 1 partie 1

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,25 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairClicked = 0
+    this.pairsGuessed= 0;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
+  shuffleCards() {
+    var i, tmp;
+    console.log(i, tmp)
+    for (i = this.cards.length - 1; i > 0; i--) {
+        randomIndex = Math.floor(Math.random() * (i + 1));
+        console.log(randomIndex)
+        tmp = this.cards[i];
+        console.log(this.cards[i])
+        this.cards[i] = this.cards[randomIndex];
+        this.cards[randomIndex] = tmp;
+    }
+    return this.cards;
+    
+  }
   checkIfPair(card1, card2) {}
   isFinished() {}
 }
